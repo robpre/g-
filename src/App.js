88204bea0484302b7e46249c2fc8e057cec0bd3b
Robert Preus-MacLaren <rob@neontribe.co.uk>
2018-11-16 21:57:45 +0000

    add a control!?

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { Router, Route, Link, Switch } from "react-router-dom";
 
 import FullscreenMap from './components/FullscreenMap/FullscreenMap';
 import Search from "./components/Search/Search";
-
+import { createBrowserHistory } from 'history';
 import List from './components/List/List';
+const history = createBrowserHistory();
+
+const existing = localStorage.getItem('active-walk');
 
 class App extends Component {
   state = {
-    active: null
+    active: existing && JSON.parse(existing),
   };
 
   selectRoute = (walk) => {
     this.setState({active: walk});
+    localStorage.setItem('active-walk', JSON.stringify(walk));
+    history.push('/map');
   };
 
   render() {
     return (
       <div style={{height: '100%', width: '100%'}}>
-        <Router>
+        <Router history={history}>
           <Switch style={{ height: '100%', width: '100%'}}>
-            <Route path="/">
+            <Route path="/" exact>
               <List onSelect={this.selectRoute} />
             </Route>
             <Route path="/map">
-              {() => (
-                <FullscreenMap route={this.state.active && this.state.active.path} />
+              {(p) => (console.log(this.state.active) ||
+                <FullscreenMap centerRoute route={this.state.active && this.state.active.path} />
               )}
             </Route>
             <Route path="/search">
--- src/components/FullscreenMap/FullscreenMap.js
+++ src/components/FullscreenMap/FullscreenMap.js
@@ -2,9 +2,23 @@ import React from 'react';
 
 import Map from '../Map/Map';
 
+import Control from 'react-leaflet-control';
+
+const style = {
+    position: 'absolute',
+    zIndex: '1000',
+    pointerEvents: 'none',
+    top: '50%', /* possible because the placeholder's parent is the map */
+    transform: 'translateY(-50%)', /* using the CSS3 Transform technique */
+    paddingTop: '10px',
+};
+
 export default (props) => (
     <div style={{ width: '100%', height: '100%'}}>
         <Map {...props}>
+            <Control position="bottomleft">
+                <h1>asdjasd</h1>
+            </Control>
         </Map>
     </div>
 );
