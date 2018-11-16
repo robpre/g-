76455f827745423cad2ce772eec8b5617f09a82f
Robert Preus-MacLaren <rob@neontribe.co.uk>
2018-11-16 11:28:50 +0000

    added a map

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './map.css';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+L.Marker.prototype.options.icon = DefaultIcon;
+
+const position = [51.505, -0.09]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{height: 500, width: 500}} className="map">
+        <Map center={position} zoom={13}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          />
+          <Marker position={position}>
+            <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+          </Marker>
+        </Map>
       </div>
     );
   }
 }
+
+
 
 export default App;
